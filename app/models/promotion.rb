511c1373fb85dba51067f2965d5471a8bf4eaabb
Henry Diaz <henryalberto.diaz@gmail.com>
2013-09-01 17:58:06 -0600

    modified attachment class_name in event and promotions

--- app/models/promotion.rb
+++ app/models/promotion.rb
@@ -2,5 +2,5 @@ class Promotion < ActiveRecord::Base
   attr_accessible :name, :description, :start_at, :end_at, :price
 
   # relations
-  has_one :picture, class_name: :attachment, as: :attachmentable, dependendent: :destroy
+  has_one :picture, class_name: "Attachment", as: :attachmentable, dependendent: :destroy
 end
