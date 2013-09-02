b89cf303bdb3f07e934c10637f727609380461da
Henry Diaz <henryalberto.diaz@gmail.com>
2013-09-01 18:33:27 -0600

    change mispelled dependent keyword

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,5 +2,5 @@ class Event < ActiveRecord::Base
   attr_accessible :name, :description, :picture, :start_at, :end_at
 
   # relations
-  has_one :picture, class_name: "Attachment", as: :attachmentable, dependendent: :destroy
+  has_one :picture, class_name: "Attachment", as: :attachmentable, dependent: :destroy
 end
